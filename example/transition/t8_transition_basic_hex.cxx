628c51c8194219a0da3bbc08bc0e8e56cb441c07
Tabea Leistikow <tabea.leistikow@dlr.de>
2023-07-13 16:13:26 +0200

    back up

---
+++ example/transition/t8_transition_basic_hex.cxx
@@ -0,0 +1,134 @@
+/*
+  This file is part of t8code.
+  t8code is a C library to manage a collection (a forest) of multiple
+  connected adaptive space-trees of general element types in parallel.
+
+  Copyright (C) 2015 the developers
+
+  t8code is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  t8code is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with t8code; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+/* This is step3 of the t8code tutorials.
+ * After generating a coarse mesh (step1) and building a uniform forest
+ * on it (step2), we will now adapt (= refine and coarsen) the forest
+ * according to our own criterion.
+ * 
+ * The geometry (coarse mesh) is again a cube, this time modelled with
+ * 6 tetrahedra, 6 prisms and 4 cubes.
+ * We refine an element if its midpoint is whithin a sphere of given radius
+ * around the point (0.5, 0.5, 1) and we coarsen outside of a given radius.
+ * We will use non-recursive refinement, that means that the refinement level
+ * of any element will change by at most +-1.
+ * 
+ * How you can experiment here:
+ *   - Look at the paraview output files of the unifomr and the adapted forest.
+ *     For the adapted forest you can apply a slice filter to look into the cube.
+ *   - Run the program with different process numbers. You should see that refining is
+ *     independent of the number of processes, but coarsening is not.
+ *     This is due to the face that a family can only be coarsened if it is completely
+ *     local to a single process and the distribution among the process may break this property.
+ *   - Change the midpoint coordinates and the radii.
+ *   - Change the adaptation criterion such that elements inside the sphere are coarsened
+ *     and elements outside are refined.
+ *   - Use t8_productionf to print the local number of elements on each process.
+ *     Notice, that the uniform forest is evenly distributed, but that the adapted forest
+ *     is not. This is due to the fact that we do not repartition our forest here.
+ *   - Add a maximum refinement level to the adapt_data struct and use non-recursive refinement.
+ *     Do not refine an element if it has reached the maximum level. (Hint: ts->t8_element_level)
+ */
+
+#include <t8.h>                 /* General t8code header, always include this. */
+#include <t8_cmesh.h>           /* cmesh definition and basic interface. */
+#include <t8_cmesh/t8_cmesh_examples.h> /* A collection of exemplary cmeshes */
+#include <t8_forest/t8_forest_general.h>        /* forest definition and basic interface. */
+#include <t8_forest/t8_forest_io.h>     /* save forest */
+#include <t8_forest/t8_forest_geometrical.h>    /* geometrical information of the forest */
+#include <t8_schemes/t8_default/t8_default_cxx.hxx>     /* default refinement scheme. */
+
+T8_EXTERN_C_BEGIN ();
+
+
+int
+t8_adapt_callback (t8_forest_t forest,
+                         t8_forest_t forest_from,
+                         t8_locidx_t which_tree,
+                         t8_locidx_t lelement_id,
+                         t8_eclass_scheme_c *ts,
+                         const int is_family,
+                         const int num_elements, t8_element_t *elements[])
+{
+  if (which_tree == 0 && lelement_id == 0) {
+    return 1;
+  }
+  /* Do not change this element. */
+  return 0;
+}
+
+
+int
+main (int argc, char **argv)
+{
+  int                 mpiret;
+  sc_MPI_Comm         comm;
+  t8_cmesh_t          cmesh;
+  t8_forest_t         forest;
+
+
+  /* Initialize MPI. This has to happen before we initialize sc or t8code. */
+  mpiret = sc_MPI_Init (&argc, &argv);
+  /* Error check the MPI return value. */
+  SC_CHECK_MPI (mpiret);
+
+  /* Initialize the sc library, has to happen before we initialize t8code. */
+  sc_init (sc_MPI_COMM_WORLD, 1, 1, NULL, SC_LP_ESSENTIAL);
+  /* Initialize t8code with log level SC_LP_PRODUCTION. See sc.h for more info on the log levels. */
+  t8_init (SC_LP_PRODUCTION);
+
+
+  /* We will use MPI_COMM_WORLD as a communicator. */
+  comm = sc_MPI_COMM_WORLD;
+
+  /*
+   * Setup.
+   * Build cmesh and uniform forest.
+   */
+
+  /* Build a cube cmesh with tet, hex, and prism trees. */
+  cmesh = t8_cmesh_new_from_class (T8_ECLASS_HEX, comm);
+  const int           level = 3;
+  forest = t8_forest_new_uniform (cmesh, t8_scheme_new_default_cxx (), level, 0, comm);
+
+
+  /*
+   *  Adapt the forest.
+   */
+  forest = t8_forest_new_adapt (forest, t8_adapt_callback, 0, 0, NULL);
+  t8_forest_write_vtk (forest, "t8_lalalal");
+
+  /*
+   * clean-up
+   */
+
+  /* Destroy the forest. */
+  t8_forest_unref (&forest);
+  sc_finalize ();
+
+  mpiret = sc_MPI_Finalize ();
+  SC_CHECK_MPI (mpiret);
+
+  return 0;
+}
+
+T8_EXTERN_C_END ();
